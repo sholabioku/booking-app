27c1fb8f53d09c1364198f10b141fcb7e4629406
sholabioku <bilushonline@gmail.com>
2022-08-16 14:59:31 +0100

    Add functionality for Hotel schema

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -2,4 +2,11 @@ import express from 'express';
 
 const router = express.Router();
 
+// @desc Create Hotel
+// @route POST /api/v1/hotels
+// @access Private
+router.post('/', (req, res) => {
+  res.send('Create Hotel');
+});
+
 export default router;
