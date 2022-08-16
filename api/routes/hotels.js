dfe0ae8bffe0ba095a4aa2cd867a1ac814829281
sholabioku <bilushonline@gmail.com>
2022-08-16 17:11:23 +0100

    Add functionality for PUT /api/v1/hotels/:id route

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -16,4 +16,23 @@ router.post('/', async (req, res) => {
   }
 });
 
+// @desc Update Hotel
+// @route PUT /api/v1/hotels/:id
+// @access Private
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 export default router;
