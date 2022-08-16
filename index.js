ae24f8b1f2c7166289109aab8fa3a4dac8274516
sholabioku <bilushonline@gmail.com>
2022-08-16 12:10:25 +0100

    Install nodemon and colors packages

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import colors from 'colors';
 
 const app = express();
 
 app.listen(8800, () => {
-  console.log('Server is running on port 8800');
+  console.log('Server is running on port 8800'.yellow.bold);
 });
