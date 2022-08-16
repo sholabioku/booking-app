f4a28e86d6838db3afdd69f7dde752f2843bf64a
sholabioku <bilushonline@gmail.com>
2022-08-16 12:31:07 +0100

    Add functionality for connection to mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import express from 'express';
+import mongoos from 'mongoose';
 import colors from 'colors';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoos.connect(process.env.MONGO_URI);
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+    );
+  } catch (err) {
+    console.log(err.red.bold);
+    process.exit(1);
+  }
+};
 
 app.listen(8800, () => {
+  connectDB();
   console.log('Server is running on port 8800'.yellow.bold);
 });
