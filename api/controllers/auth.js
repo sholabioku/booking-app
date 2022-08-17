1f3ac57f862d81bcff83c1ff60c411831f503762
sholabioku <bilushonline@gmail.com>
2022-08-17 17:21:24 +0100

    Install bcrypt and add functionality to hash user password

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,5 @@
 import asyncHandler from 'express-async-handler';
-
+import bcrypt from 'bcryptjs';
 import User from '../models/User.js';
 
 // @desc Register User
@@ -7,7 +7,10 @@ import User from '../models/User.js';
 // @access Public
 export const register = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const newUser = new User({ username, email, password: hashedPassword });
   await newUser.save();
   res.status(201).json('User has been created');
 });
