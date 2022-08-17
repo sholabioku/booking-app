c35391cb979da7d868a95b29d13fc3ee10161e7a
sholabioku <bilushonline@gmail.com>
2022-08-17 17:14:23 +0100

    Add functionality for POST /api/v1/auth/register route

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,13 @@
 import asyncHandler from 'express-async-handler';
 
+import User from '../models/User.js';
+
 // @desc Register User
 // @route POST /api/v1/auth/register
 // @access Public
-export const register = asyncHandler(async (req, res) => {});
+export const register = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  const newUser = new User({ username, email, password });
+  await newUser.save();
+  res.status(201).json('User has been created');
+});
