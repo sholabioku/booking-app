b4db033758921252cffed0aa10fc73d72ec914f6
sholabioku <bilushonline@gmail.com>
2022-08-19 09:49:30 +0100

    Add functionality for DELETE /api/v1/rooms/:id/:hotelId route

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -1,6 +1,6 @@
 import asyncHandler from 'express-async-handler';
-import Hotel from '../models/Hotel';
-import Room from '../models/Room';
+import Hotel from '../models/Hotel.js';
+import Room from '../models/Room.js';
 
 // @desc Create Room
 // @route POST /api/v1/rooms/:hotelId
@@ -14,4 +14,46 @@ export const createRoom = asyncHandler(async (req, res, next) => {
   await Hotel.findByIdAndUpdate(hotelId, { $push: { rooms: savedRoom._id } });
 
   res.status(201).json(savedRoom);
+});
+
+// @desc Update Room
+// @route PUT /api/v1/rooms/:id
+// @access Private
+export const updateRoom = asyncHandler(async (req, res) => {
+  const updatedRoom = await Room.findByIdAndUpdate(
+    req.params.id,
+    { $set: req.body },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  res.status(200).json(updatedRoom);
+});
+
+// @desc Delete Room
+// @route DELETE /api/v1/rooms/:id/:hotelId
+// @access Private
+export const deleteRoom = asyncHandler(async (req, res) => {
+  const hotelId = req.params.hotelId;
+  await Room.findByIdAndDelete(req.params.id);
+
+  await Hotel.findByIdAndUpdate(hotelId, { $pull: { rooms: req.params.id } });
+  res.status(200).json('Room has been deleted');
+});
+
+// @desc Get All Rooms
+// @route GET /api/v1/rooms
+// @access Public
+export const getRooms = asyncHandler(async (req, res, next) => {
+  const rooms = await Room.find();
+  res.status(200).json(rooms);
+});
+
+// @desc Get Room by ID
+// @route GET /api/v1/rooms/:id
+// @access Private
+export const getRoom = asyncHandler(async (req, res) => {
+  const room = await Room.findById(req.params.id);
+  res.status(200).json(rooms);
 });
--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,19 @@
 import express from 'express';
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+} from '../controllers/room.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
+
+router.post('/:hotelId', verifyAdmin, createRoom);
+router.put('/:id', verifyAdmin, updateRoom);
+router.delete('/:id/:hotelId', verifyAdmin, deleteRoom);
+router.get('/', getRooms);
+router.get('/:id', getRoom);
 
 export default router;
