f81a0c7a3de1c9c579de7e54763523ea055c4d29
sholabioku <bilushonline@gmail.com>
2022-09-01 15:09:36 +0100

    Protect admin routes

--- admin/src/App.js
+++ admin/src/App.js
@@ -37,19 +37,55 @@ function App() {
               }
             />
             <Route path='users'>
-              <Route index element={<List />} />
-              <Route path=':userId' element={<Single />} />
+              <Route
+                index
+                element={
+                  <ProtectedRoute>
+                    <List />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path=':userId'
+                element={
+                  <ProtectedRoute>
+                    <Single />
+                  </ProtectedRoute>
+                }
+              />
               <Route
                 path='new'
-                element={<New inputs={userInputs} title='Add New User' />}
+                element={
+                  <ProtectedRoute>
+                    <New inputs={userInputs} title='Add New User' />
+                  </ProtectedRoute>
+                }
               />
             </Route>
             <Route path='products'>
-              <Route index element={<List />} />
-              <Route path=':productId' element={<Single />} />
+              <Route
+                index
+                element={
+                  <ProtectedRoute>
+                    <List />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path=':productId'
+                element={
+                  <ProtectedRoute>
+                    <Single />
+                  </ProtectedRoute>
+                }
+              />
               <Route
                 path='new'
-                element={<New inputs={productInputs} title='Add New Product' />}
+                element={
+                  <ProtectedRoute>
+                    <New inputs={productInputs} title='Add New Product' />
+                  </ProtectedRoute>
+                }
               />
             </Route>
           </Route>
