2d1619b5b3cd5519742883814f9741932596044b
sholabioku <bilushonline@gmail.com>
2022-09-02 12:01:23 +0100

    Add functionality to make ajax request for DELETE /api/users/:id

--- admin/src/components/datatable/Datatable.jsx
+++ admin/src/components/datatable/Datatable.jsx
@@ -1,17 +1,28 @@
 import './datatable.scss';
 import { DataGrid } from '@mui/x-data-grid';
 import { userColumns, userRows } from '../../datatablesource';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import useFetch from '../../hooks/useFetch';
+import axios from 'axios';
 
 const Datatable = () => {
-  // const [data, setData] = useState(userRows);
+  const location = useLocation();
+  const path = location.pathname.split('/')[1];
 
-  const { data, loading, error } = useFetch('/users');
+  const [list, setList] = useState([]);
 
-  const handleDelete = (id) => {
-    // setData(data.filter((item) => item.id !== id));
+  const { data, loading, error } = useFetch(`/${path}`);
+
+  useEffect(() => {
+    setList(data);
+  }, [data]);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`/${path}/${id}`);
+      setList(list.filter((item) => item._id !== id));
+    } catch (error) {}
   };
 
   const actionColumn = [
@@ -27,7 +38,7 @@ const Datatable = () => {
             </Link>
             <div
               className='deleteButton'
-              onClick={() => handleDelete(params.row.id)}
+              onClick={() => handleDelete(params.row._id)}
             >
               Delete
             </div>
@@ -46,7 +57,7 @@ const Datatable = () => {
       </div>
       <DataGrid
         className='datagrid'
-        rows={data}
+        rows={list}
         columns={userColumns.concat(actionColumn)}
         pageSize={9}
         rowsPerPageOptions={[9]}
