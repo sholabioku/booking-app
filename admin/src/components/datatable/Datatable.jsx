21f17e3bef4dfcf0717f74dff462751c9dc57d9c
sholabioku <bilushonline@gmail.com>
2022-09-02 11:31:27 +0100

    Add functionality to make ajax request for GET /api/users

--- admin/src/components/datatable/Datatable.jsx
+++ admin/src/components/datatable/Datatable.jsx
@@ -1,29 +1,32 @@
-import "./datatable.scss";
-import { DataGrid } from "@mui/x-data-grid";
-import { userColumns, userRows } from "../../datatablesource";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import './datatable.scss';
+import { DataGrid } from '@mui/x-data-grid';
+import { userColumns, userRows } from '../../datatablesource';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import useFetch from '../../hooks/useFetch';
 
 const Datatable = () => {
-  const [data, setData] = useState(userRows);
+  // const [data, setData] = useState(userRows);
+
+  const { data, loading, error } = useFetch('/users');
 
   const handleDelete = (id) => {
-    setData(data.filter((item) => item.id !== id));
+    // setData(data.filter((item) => item.id !== id));
   };
 
   const actionColumn = [
     {
-      field: "action",
-      headerName: "Action",
+      field: 'action',
+      headerName: 'Action',
       width: 200,
       renderCell: (params) => {
         return (
-          <div className="cellAction">
-            <Link to="/users/test" style={{ textDecoration: "none" }}>
-              <div className="viewButton">View</div>
+          <div className='cellAction'>
+            <Link to='/users/test' style={{ textDecoration: 'none' }}>
+              <div className='viewButton'>View</div>
             </Link>
             <div
-              className="deleteButton"
+              className='deleteButton'
               onClick={() => handleDelete(params.row.id)}
             >
               Delete
@@ -34,20 +37,21 @@ const Datatable = () => {
     },
   ];
   return (
-    <div className="datatable">
-      <div className="datatableTitle">
+    <div className='datatable'>
+      <div className='datatableTitle'>
         Add New User
-        <Link to="/users/new" className="link">
+        <Link to='/users/new' className='link'>
           Add New
         </Link>
       </div>
       <DataGrid
-        className="datagrid"
+        className='datagrid'
         rows={data}
         columns={userColumns.concat(actionColumn)}
         pageSize={9}
         rowsPerPageOptions={[9]}
         checkboxSelection
+        getRowId={(row) => row._id}
       />
     </div>
   );
--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -9,12 +9,10 @@ import { createError } from '../utils/error.js';
 // @route POST /api/v1/auth/register
 // @access Public
 export const register = asyncHandler(async (req, res) => {
-  const { username, email, password } = req.body;
-
   const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-  const newUser = new User({ username, email, password: hashedPassword });
+  const newUser = new User({ ...req.body, password: hashedPassword });
   await newUser.save();
 
   res.status(201).json('User has been created');
