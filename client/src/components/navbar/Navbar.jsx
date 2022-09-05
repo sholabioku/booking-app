70650e1fb12480afcd2fe251564019f5d0b6ce09
sholabioku <bilushonline@gmail.com>
2022-09-05 07:52:37 +0100

    Navigate

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -27,12 +27,12 @@ const Navbar = () => {
           </div>
         ) : (
           <div className='navItems'>
-            <button className='navButton' onClick={() => navigate('/register')}>
-              Register
-            </button>
-            <button className='navButton' onClick={() => navigate('/login')}>
-              Login
-            </button>
+            <Link to='/register'>
+              <button className='navButton'>Register</button>
+            </Link>
+            <Link to='/login'>
+              <button className='navButton'>Login</button>
+            </Link>
           </div>
         )}
       </div>
